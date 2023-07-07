ca972dfc9f5cf567b57cd3195825723171af7927
Jblemur02 <129120303+Jblemur02@users.noreply.github.com>
2023-07-07 01:55:55 -0400

    Update contacts.py
    Added main functionality to creating contacts. Still tons of work to do.

--- contacts.py
+++ contacts.py
@@ -1,18 +1,29 @@
 class contact:
-    firstname = None
-    lastname = None
-    birthday = None
-    phone = None
-    email = None
-    address = None
-    Notes: None
-
-#def addcontacts():
-#    whotoadd = input("Who would you like to add to your contacts book?")
-#    with open(filen,"r") as file:
-#        for line in name:
-#            print(line)
-
+    def __init__(self,firstname,lastname,birthday,phone,email,address,notes):
+        self.birthday = birthday
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.notes: notes
+        self.firstname = firstname
+        self.lastname = lastname
+    
+def addcontacts():
+    print("Ok we can do that. If you don't have a certain peice of information just press enter.")
+    newcontact = contact(None,None,None,None,None,None,None)
+    whotoadd = input("What is the first name of the person?")
+    newcontact.firstname = whotoadd
+    whotoadd = input("What is the last name of the person?")
+    newcontact.lastname = whotoadd
+    whotoadd = input("What is the address of the person?")
+    newcontact.address = whotoadd
+    whotoadd = input("What is the email of the person?")
+    newcontact.email = whotoadd
+    whotoadd = input("What is the phone number of the person?")
+    newcontact.phone = whotoadd
+    whotoadd = input("Would you like to leave any notes about this person")
+    newcontact.notes = whotoadd
+    
 def readcontacts():
     whotocheck = input("Who's information would you like to retrieve? \n")
     with open(filen,"w") as file:
@@ -22,13 +33,9 @@ def readcontacts():
 
 print("Hello welcome to the contacts book")
 
-
 name = input("What is your name?\n"); 
 filen = name; filen += ".csv"; filen = filen.lower()
-with open(filen,"r") as file:
-    for line in name:
-        print(line)
-        
+
 whattodo = None
 whattodo = input("Hello "+name+" what would you like to do? \n1.Check contacts \n2.Add contacts \n3.Exit \n")
 while whattodo != "1" and whattodo != "2" and whattodo != "3": 
